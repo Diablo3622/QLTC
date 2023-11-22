1d4df93abf9d6b646e49b81525b319e6b609fc9b
Diablo3622 <datnguyen125202@gmail.com>
2023-11-22 15:54:34 +0700

    update MenuScreen

---
+++ app/src/main/java/com/example/quanlythuchi/MenuScreen.java
@@ -0,0 +1,48 @@
+package com.example.quanlythuchi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+
+import com.example.qltc.R;
+
+public class MenuScreen extends Activity {
+
+    GridView gridView;
+
+    static final String[] MOBILE_OS = new String[] {
+            "Thể Loại Thu/Chi", "Khoản Thu", "Khoản Chi","Thống Kê", "Lương" };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_screen);
+
+        gridView = (GridView) findViewById(R.id.gridViewMenu);
+
+
+
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                switch (position) {
+
+
+                }
+
+//				Toast.makeText(
+//				   getApplicationContext(),
+//				   ((TextView) v.findViewById(R.id.grid_item_label))
+//				   .getText(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+}
